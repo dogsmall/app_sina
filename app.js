ffbd5e14df122c6979446802d0c85cafe0115399
dogsmall <316211030@qq.com>
2016-07-17 13:05:11 +0800

    gsw

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var cheerio = require('cheerio')
 var fs = require('fs')
 var async = require('async')
 
-
+/**
+ * 获取基本信息
+ */
 function getinfo(url, callback) {
   let options = {
     url: url,
@@ -131,7 +133,9 @@ function getinfo(url, callback) {
   })
 }
 
-
+/**
+ * 获取获奖情况
+ */
 function getawaeds(movie, callback) {
   var options = {
     url: movie.url + "/awards/",
@@ -155,12 +159,14 @@ function getawaeds(movie, callback) {
       awards.push(award)
     }
     movie.awards.push(awards)
-    callback(null,movie)
+    callback(null, movie)
   })
 
 }
 
-
+/**
+ * 获取图片
+ */
 
 function getpics(movie, callback) {
   let options = {
@@ -194,9 +200,11 @@ function getpics(movie, callback) {
   })
 }
 
+/**
+ * 获取影评
+ */
 
-
-function getfilmrevinfo(movie, nexturl, cb) {
+function getlongcomments(movie, nexturl, cb) {
   let options
   if (nexturl) {
     options = nexturl
@@ -210,71 +218,124 @@ function getfilmrevinfo(movie, nexturl, cb) {
       }
     }
   }
+  request(options, function (err, data) {
+    let $ = cheerio.load(data.body);
+    let objs = Array.from($('#content > div > div.article > div:nth-child(2)>div.review'))
+    for (let i = 0; i < objs.length; i++) {
+      setTimeout(function () {
+        request(objs[i].childNodes[1].childNodes[1].childNodes[5].attribs.href, function (err, data) {
+          let $ = cheerio.load(data.body)
+          let filmrev = {
+            longcomments_url: options.url,
+            title: $('#content>h1>span').text(),
+            author: {
+              name: $("#content > div > div.article > div > div > div.main-hd > p > a:nth-child(2)").text().trim(),
+              url: $("#content > div > div.article > div > div > div.main-hd > p > a:nth-child(2)").attr('href'),
+              imgurl: $("#content > div > div.article > div > div > div.main-hd > p > a.main-avatar > img").attr('src')
+            },
+            time: $("#content > div > div.article > div > div > div.main-hd > p:nth-child(1) > span.main-meta").text(),
+            content: $("#link-report > div").text(),
+            grade: $("#content > div > div.article > div > div > div.main-hd > p:nth-child(1) > span.main-title-rating").attr("title"),
 
-  setTimeout(function () {
-    console.log(options)
-    request(options, function (err, data) {
-      let $ = cheerio.load(data.body);
-      let objs = Array.from($('#content > div > div.article > div:nth-child(2)>div.review'))
-      for (let i = 0; i < objs.length; i++) {
-        setTimeout(function () {
-          request(objs[i].childNodes[1].childNodes[1].childNodes[5].attribs.href, function (err, data) {
-            let $ = cheerio.load(data.body)
-            let filmrev = {
-              longcomments_url: options.url,
-              title: $('#content>h1>span').text(),
-              author: {
-                name: $("#content > div > div.article > div > div > div.main-hd > p > a:nth-child(2)").text().trim(),
-                url: $("#content > div > div.article > div > div > div.main-hd > p > a:nth-child(2)").attr('href'),
-                imgurl: $("#content > div > div.article > div > div > div.main-hd > p > a.main-avatar > img").attr('src')
-              },
-              time: $("#content > div > div.article > div > div > div.main-hd > p:nth-child(1) > span.main-meta").text(),
-              content: $("#link-report > div").text(),
-              grade: $("#content > div > div.article > div > div > div.main-hd > p:nth-child(1) > span.main-title-rating").attr("title"),
-
-              agree: $("#content > div > div.article > div > div > div.main-ft > div.main-panel > div.main-panel-useful").children().first().children().last().text(),
-              disagree: $("#content > div > div.article > div > div > div.main-ft > div.main-panel > div.main-panel-useful").children().last().children().last().text(),
-              comment: []
-            }
-            console.log("11111")
-            // console.log(value.childNodes[1].childNodes[1].childNodes[5].attribs.href)
-            movie.longcomments.push(filmrev)
-            // let options = {
-            //     url: value.childNodes[1].childNodes[1].childNodes[5].attribs.href,
-            //     headers: {
-            //         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
-            //         Cookie: ' ll="108288"; bid=vLofpXemIo0; __utma=30149280.798883795.1467016530.1467077534.1467077534.5; __utmz=30149280.1467077534.5.4.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utma=223695111.1460687376.1467016532.1467020867.1467077534.3; __utmz=223695111.1467077534.3.2.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1467077532%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_id.100001.4cf6=fab42eaaeed692a8.1467016534.3.1467077795.1467020865.; ap=1; ps=y; push_noty_num=0; push_doumail_num=5; __utmv=30149280.13744; _pk_ses.100001.4cf6=*; __utmb=30149280.7.10.1467077534; __utmc=30149280; __utmb=223695111.0.10.1467077534; __utmc=223695111; __utmt=1; ue="316211030@qq.com"; dbcl2="137448756:e9/xvl5xGp4"; ck=LV5q'
-            //     }
-            // }
-            // filmrevcomment(options, movie)
-            // console.log("55555")
-          })
-        }, i * 2000);
-      }
-
-      if ($('#paginator > a.next').attr('href')) {
-        console.log("影评还有下一页")
-        let nexturl = {
-          url: movie.url + "/reviews" + $('#paginator > a.next').attr('href'),
-          headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
-            Cookie: ' ll="108288"; bid=vLofpXemIo0; __utma=30149280.798883795.1467016530.1467016530.1467016875.2; __utmc=30149280; __utmz=30149280.1467016875.2.2.utmcsr=sns-nav|utmccn=(not%20set)|utmcmd=douban; __utma=223695111.1460687376.1467016532.1467016532.1467016532.1; __utmc=223695111; __utmz=223695111.1467016532.1.1.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1467016534%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_id.100001.4cf6=fab42eaaeed692a8.1467016534.1.1467016918.1467016534.; ap=1; ps=y; dbcl2="137448756:e9/xvl5xGp4"; ck=LV5q; push_noty_num=0; push_doumail_num=5; __utmv=30149280.13744'
+            agree: $("#content > div > div.article > div > div > div.main-ft > div.main-panel > div.main-panel-useful").children().first().children().last().text(),
+            disagree: $("#content > div > div.article > div > div > div.main-ft > div.main-panel > div.main-panel-useful").children().last().children().last().text(),
+            comment: []
           }
-        }
-        return getfilmrevinfo(movie, nexturl)
+          movie.longcomments.push(filmrev)
+          // let options = {
+          //     url: value.childNodes[1].childNodes[1].childNodes[5].attribs.href,
+          //     headers: {
+          //         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
+          //         Cookie: ' ll="108288"; bid=vLofpXemIo0; __utma=30149280.798883795.1467016530.1467077534.1467077534.5; __utmz=30149280.1467077534.5.4.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utma=223695111.1460687376.1467016532.1467020867.1467077534.3; __utmz=223695111.1467077534.3.2.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1467077532%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_id.100001.4cf6=fab42eaaeed692a8.1467016534.3.1467077795.1467020865.; ap=1; ps=y; push_noty_num=0; push_doumail_num=5; __utmv=30149280.13744; _pk_ses.100001.4cf6=*; __utmb=30149280.7.10.1467077534; __utmc=30149280; __utmb=223695111.0.10.1467077534; __utmc=223695111; __utmt=1; ue="316211030@qq.com"; dbcl2="137448756:e9/xvl5xGp4"; ck=LV5q'
+          //     }
+          // }
+          // filmrevcomment(options, movie)
+          // console.log("55555")
+        })
+      }, i * 2000);
+    }
 
+    hasnext($, reviews, function (err, movie, nexturl) {
+      if (err) {
+        console.log(err)
+      }
+      if (nexturl) {
+        getlongcomments(movie, nexturl)
       } else {
-        console.log("这部电影的影评已经爬完")
-        cb(null, movie)
+        callback(null, movie)
       }
     })
-  }, 50000);
+  })
 
 }
 
 
+/**
+ * 获取短评
+ */
+
+function getshortcomments(movie, nexturl, callback) {
+  var options = {
+    url: movie.url + '/comments',
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
+      Cookie: ' ll="108288"; bid=vLofpXemIo0; __utma=30149280.798883795.1467016530.1467016530.1467016875.2; __utmc=30149280; __utmz=30149280.1467016875.2.2.utmcsr=sns-nav|utmccn=(not%20set)|utmcmd=douban; __utma=223695111.1460687376.1467016532.1467016532.1467016532.1; __utmc=223695111; __utmz=223695111.1467016532.1.1.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1467016534%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_id.100001.4cf6=fab42eaaeed692a8.1467016534.1.1467016918.1467016534.; ap=1; ps=y; dbcl2="137448756:e9/xvl5xGp4"; ck=LV5q; push_noty_num=0; push_doumail_num=5; __utmv=30149280.13744'
+    }
+  }
+  request(options, function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(data.body)
+
+    let $ = cheerio.load(data.body)
+    let objs = Array.from($('#comments>div.comment-item'));
+    console.log(items)
+    let shortcomments = []
+    for (let value of objs) {
+      let comment = {
+        name: value.childNodes[1].childNodes[1].attribs.title,
+        nameurl: value.childNodes[1].childNodes[1].attribs.href,
+        picurl: value.childNodes[1].childNodes[1].childNodes[1].attribs.src,
+        content: value.childNodes[3].childNodes[3].childNodes[0].data,
+        agree: value.childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes[0].data,
+        grade: value.childNodes[3].childNodes[1].childNodes[3].childNodes[3].attribs.title,
+        time: value.childNodes[3].childNodes[1].childNodes[3].childNodes[5].childNodes[0].data.trim(),
+
+      }
+      shortcomments.push(comment)
+      console.log(comment)
+    }
+    movie.shortcomments.push(shortcomments)
+    hasnext($)
+  })
+}
+
+/**
+ * 是否有下一页
+ */
+function hasnext($, channel, callback) {
+  if ($('#paginator > a.next').attr('href')) {
+    console.log("影评还有下一页")
+    let nexturl = {
+      url: movie.url + "/" + channel + $('#paginator > a.next').attr('href'),
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
+        Cookie: ' ll="108288"; bid=vLofpXemIo0; __utma=30149280.798883795.1467016530.1467016530.1467016875.2; __utmc=30149280; __utmz=30149280.1467016875.2.2.utmcsr=sns-nav|utmccn=(not%20set)|utmcmd=douban; __utma=223695111.1460687376.1467016532.1467016532.1467016532.1; __utmc=223695111; __utmz=223695111.1467016532.1.1.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1467016534%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_id.100001.4cf6=fab42eaaeed692a8.1467016534.1.1467016918.1467016534.; ap=1; ps=y; dbcl2="137448756:e9/xvl5xGp4"; ck=LV5q; push_noty_num=0; push_doumail_num=5; __utmv=30149280.13744'
+      }
+    }
+    callback(null, movie, nexturl)
+    // return getfilmrevinfo(movie, nexturl)
+
+  } else {
+    console.log("这部电影的影评已经爬完")
+    cb(null, movie, null)
+  }
+}
+
+
 getinfo("https://movie.douban.com/subject/25862357", function (err, movie) {
-  if(err){
+  if (err) {
     console.log(err)
   }
   getfilmrevinfo(movie, null, function (err, movie) {
