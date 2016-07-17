d1f734728a6bfa5707b94730238a9208ec5777d0
dogsmall <316211030@qq.com>
2016-07-17 11:07:32 +0800

    添加了获奖情况

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var request = require('request').defaults({
   jar: true
 })
 var cheerio = require('cheerio')
-
+var fs = require('fs')
+var async = require('async')
 
 
 function getinfo(url, callback) {
@@ -131,6 +132,34 @@ function getinfo(url, callback) {
 }
 
 
+function getawaeds(movie, callback) {
+  var options = {
+    url: movie.url + "/awards/",
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
+      Cookie: ' ll="108288"; bid=vLofpXemIo0; __utma=30149280.798883795.1467016530.1467016530.1467016875.2; __utmc=30149280; __utmz=30149280.1467016875.2.2.utmcsr=sns-nav|utmccn=(not%20set)|utmcmd=douban; __utma=223695111.1460687376.1467016532.1467016532.1467016532.1; __utmc=223695111; __utmz=223695111.1467016532.1.1.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1467016534%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_id.100001.4cf6=fab42eaaeed692a8.1467016534.1.1467016918.1467016534.; ap=1; ps=y; dbcl2="137448756:e9/xvl5xGp4"; ck=LV5q; push_noty_num=0; push_doumail_num=5; __utmv=30149280.13744'
+
+    }
+  }
+
+  request(options, function (err, data) {
+    let $ = cheerio.load(data.body);
+    let objs = Array.from($("#content > div > div.article>div.awards"));
+    let awards = []
+    for (let value of objs) {
+      var award = {
+        name: value.childNodes[1].childNodes[1].childNodes[1].childNodes[0].data,
+        url: value.childNodes[1].childNodes[1].childNodes[1].attribs.href,
+        info: $(value).find("ul.award").text().trim()
+      }
+      awards.push(award)
+    }
+    movie.awards.push(awards)
+    callback(null,movie)
+  })
+
+}
+
 
 
 function getpics(movie, callback) {
@@ -159,8 +188,9 @@ function getpics(movie, callback) {
       pictuers.push(pic)
       // console.log(pic)
     }
-    movie.pic = pictuers
+    movie.pic.push(pictuers)
     callback(null, movie)
+
   })
 }
 
@@ -238,12 +268,15 @@ function getfilmrevinfo(movie, nexturl, cb) {
         cb(null, movie)
       }
     })
-  }, 25000);
+  }, 50000);
 
 }
 
 
 getinfo("https://movie.douban.com/subject/25862357", function (err, movie) {
+  if(err){
+    console.log(err)
+  }
   getfilmrevinfo(movie, null, function (err, movie) {
     if (err) {
       console.log(err)
